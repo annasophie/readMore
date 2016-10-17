1e9cbe7353dd7b63a9810d67e584c774a4d0f4e8
annasophie <annasophie.nagel@gmail.com>
2016-10-17 16:26:46 -0400

    changed serve style

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var ip = '127.0.0.1'
 app.use(express.static(__dirname + '/client'));
 
 app.get('/', function(request, response) {
-  response.render('/Client/index.html');
+  response.sendFile(__dirname + '/Client/index.html');
 });
 
 app.listen(port, function() {
