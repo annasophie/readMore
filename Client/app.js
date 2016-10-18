003d94162ca4664ac766dbdba6ace94214a979af
annasophie <annasophie.nagel@gmail.com>
2016-10-18 00:54:35 -0400

    Basic one-at-a-time viewing enabled

--- Client/app.js
+++ Client/app.js
@@ -8,15 +8,31 @@ readmore.controller('ReadMoreCtrl', ['$scope', 'Books', function($scope, Books) 
       description: "DESCRIPTION"
     };
     $scope.bookList = [];
+    $scope.index = 0;
+    $scope.currentBook = {};
+    $scope.sought = false;
 
     $scope.findBooks = function(){
+      $scope.sought = true;
       Books.findBooks($scope.topic)
       .then(function(books) {
-        console.log("finding book: " + books);
         $scope.topic = '';
-        $scope.bookList = books;
+        books.items.forEach(function(book){
+          $scope.bookList.push(book);
+        });
+        $scope.currentBook = books.items[$scope.index];
       })
-    }
+    };
+
+    $scope.scroll = function() {
+      console.log('scrolling')
+      if ($scope.index < $scope.bookList.length) {
+        $scope.index++;
+        console.log($scope.index)
+        $scope.currentBook = $scope.bookList[$scope.index];
+      }
+      //TODO: add else
+    };
 }]);
 
 readmore.factory('Books', ['$http',
@@ -28,17 +44,9 @@ readmore.factory('Books', ['$http',
           method: 'GET',
           url: 'https://www.googleapis.com/books/v1/volumes?q="' + topic + '"'
         }).then(function(info){
-          // console.log(info);
-          // console.log(info.data.items[0].volumeInfo.title);
-          return info.data.items;
+          return info.data;
         })
       }
     }
   }
 ]);
-// readMore.factory('Books', function($http){});
-
-// angular.module('ngAppDemo', []).controller('ngAppDemoController', function($scope) {
-//   $scope.a = 1;
-//   $scope.b = 2;
-// });
