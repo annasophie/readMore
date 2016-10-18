1082f090919f938455e64f1756c0b6b1aa02e33b
annasophie <annasophie.nagel@gmail.com>
2016-10-17 20:23:45 -0400

    Angular updates: Added Books factory to Controller

--- Client/app.js
+++ Client/app.js
@@ -1,14 +1,29 @@
-angular.module('readmore', []).controller('ReadMoreCtrl', function($scope) {
+var readmore = angular.module('readmore', []);
+
+readmore.controller('ReadMoreCtrl', ['$scope', 'Books', function($scope, Books) {
     $scope.book = {
       title: "TITLE",
       subtitle: "SUBTITLE",
       author: "AUTHOR",
       description: "DESCRIPTION"
+    };
+    $scope.findBooks = function(){
+      Books.findBooks("HI there")
     }
-})
+}]);
 
-.factory('Books', function($http){
-  $http({
-    method: 'GET',
-  })
-});
+readmore.factory('Books', ['$http',
+  function($http){
+    return {
+      findBooks: function(topic){
+        console.log(topic);
+      }
+    }
+  }
+]);
+// readMore.factory('Books', function($http){});
+
+// angular.module('ngAppDemo', []).controller('ngAppDemoController', function($scope) {
+//   $scope.a = 1;
+//   $scope.b = 2;
+// });
